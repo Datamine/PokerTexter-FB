029d30abeef32fd819cbee74b6cf3dc0981e8f43
Datamine <contact@johnloeber.com>
2017-03-20 00:58:48 -0500

    webhook stuff

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app --log-file=- -w 4
+web: gunicorn app --log-file=-
--- app.py
+++ app.py
@@ -15,10 +15,9 @@ app = flask.Flask(__name__)
 
 # TODO: Set environment variables appropriately.
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['FACEBOOK_PAGE_ACCESS_TOKEN'] = os.environ[
-    'FACEBOOK_PAGE_ACCESS_TOKEN']
+app.config['FACEBOOK_PAGE_ACCESS_TOKEN'] = os.environ['FACEBOOK_PAGE_ACCESS_TOKEN']
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'mysecretkey')
-app.config['FACEBOOK_WEBHOOK_VERIFY_TOKEN'] = 'mysecretverifytoken'
+app.config['FACEBOOK_WEBHOOK_VERIFY_TOKEN'] = os.environ['FACEBOOK_WEBHOOK_VERIFY_TOKEN']
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
