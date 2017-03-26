a7be5b7c05109dc7d341ff159662edacfaffc5d5
Datamine <contact@johnloeber.com>
2017-03-26 15:42:11 -0700

    moved models out

--- app.py
+++ app.py
@@ -19,19 +19,6 @@ app.config['FACEBOOK_WEBHOOK_VERIFY_TOKEN'] = os.environ['FACEBOOK_WEBHOOK_VERIF
 
 db = SQLAlchemy(app)
 
-class Hand(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    # cards are '2', '3', ..., '10', 'J', ..., 'A'
-    card_one = db.Column(db.String, nullable=False)
-    card_two = db.Column(db.String, nullable=False)
-    # true if suited, false if off-suit
-    suited = db.Column(db.Boolean, nullable=False)
-    # number of OTHER players on the table.
-    players = db.Column(db.Integer, nullable=False)
-    p_win = db.Column(db.Float, nullable=False)
-    p_tie = db.Column(db.Float, nullable=False)
-    expected_gain = db.Column(db.Float, nullable=False)
-
 def handle_message(*args):
     return "foo"
 
