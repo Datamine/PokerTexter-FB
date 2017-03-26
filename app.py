d6e85a2cfb81b9c1ec71c8f7c803bd808160377e
Datamine <contact@johnloeber.com>
2017-03-26 15:18:22 -0700

    added db setup - fixed required=True snafu

--- app.py
+++ app.py
@@ -22,15 +22,15 @@ db = SQLAlchemy(app)
 class Hand(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     # cards are '2', '3', ..., '10', 'J', ..., 'A'
-    card_one = db.Column(db.String, nullable=False, required=True)
-    card_two = db.Column(db.String, nullable=False, required=True)
+    card_one = db.Column(db.String, nullable=False)
+    card_two = db.Column(db.String, nullable=False)
     # true if suited, false if off-suit
-    suited = db.Column(db.Boolean, nullable=False, required=True)
+    suited = db.Column(db.Boolean, nullable=False)
     # number of OTHER players on the table.
-    players = db.Column(db.Integer, nullable=False, required=True)
-    p_win = db.Column(db.Float, nullable=False, required=True)
-    p_tie = db.Column(db.Float, nullable=False, required=True)
-    expected_gain = db.Column(db.Float, nullable=False, required=True)
+    players = db.Column(db.Integer, nullable=False)
+    p_win = db.Column(db.Float, nullable=False)
+    p_tie = db.Column(db.Float, nullable=False)
+    expected_gain = db.Column(db.Float, nullable=False)
 
 def handle_message(*args):
     return "foo"
