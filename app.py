ade188370b0d6bfde438ac3fd997cdf4f9dbb788
Datamine <contact@johnloeber.com>
2017-03-26 17:55:19 -0700

    rank ordering

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ STANDARD_ERRORMSG = (
     "Please respond with `examples` to see some examples of correct use."
 )
 
-RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
 
 app = flask.Flask(__name__)
 
@@ -80,6 +79,7 @@ def get_stats(rank1, rank2, suiting, players):
     Query the Postgres DB for this particular Hand.
     """
 
+    ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
     # determine which of {rank1, rank2} is higher-ranked.
     # in the lookup table, hands are indexed in form RANK1 RANK2 where RANK1 <= RANK2.
     if ranks.index(rank1) <= ranks.index(rank2):
